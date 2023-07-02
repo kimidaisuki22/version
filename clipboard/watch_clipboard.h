9a0a0f7636d4bb0c444269eb329a2b7ad1700cc0
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-07-02 20:22:05 +0800

    move out clipboard.

--- clipboard/watch_clipboard.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <Windows.h>
-
-HWND nextClipboardViewer = NULL;
-
-int read_clipboard();
-LRESULT CALLBACK ClipboardViewerCallback(HWND hWnd, UINT msg, WPARAM wParam,
-                                         LPARAM lParam);
